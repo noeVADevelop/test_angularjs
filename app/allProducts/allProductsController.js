988e60f6dc3af94acab607a071e7935a6ae82185
Noe Villegas <noe.villegas.contractor@bbva.com>
2017-10-27 21:42:50 -0500

    feat(js,html): First build a table without sorting function

--- app/allProducts/allProductsController.js
+++ app/allProducts/allProductsController.js
@@ -3,8 +3,43 @@
 angular.module('store.allProducts', ['ngRoute'])
   .controller('allProductsController', allProductsController);
 
-function allProductsController(scope, http) {
+  function getAllProducts($scope,$http){
+    return $http.get(' http://demo6292426.mockable.io/more-store')
+    .then(function(result) {
+      $scope.allItems= result.data;
+     });
+  }
 
+  function sortArr($scope, prop){
+    var data = $scope.allItems || [];
+    debugger;
+    var newAllItems = [];
+    for(var i= 0; i < data.lenght; i++){
+      debugger;
+      data[i][prop].localeCompare(data[i+1][prop])
+    }
+  }
+
+function allProductsController($scope, $http) {
+  getAllProducts($scope, $http);
+  $scope.sortName = function(){
+    var data = $scope.allItems || [];
+    var newAllItems = [];
+    var compare;
+    for(var i= 0; i < data.length; i++){
+      for(var j=0; j < newAllItems.length; j++){
+        compare = data[i]['nameOfProduct'].localeCompare(newAllItems[j]['nameOfProduct']);
+        if(newAllItems.length &&newAllItems[j].hasOwnProperty('nameOfProduct') && data[i]['nameOfProduct'] !== newAllItems[j]['nameOfProduct'] && compare == -1){
+          newAllItems.splice(j,1, data[i]);
+        }else{
+          newAllItems.push(data[i]);
+        }
+      }
+    }
+    console.log(newAllItems)
+    $scope.allItems = newAllItems;
+  }
 }
+
 
 allProductsController.$inject = ['$scope', '$http'];
